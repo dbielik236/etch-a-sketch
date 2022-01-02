effb00813ae1673756da18bd1665d88b32ac0acc
David Bielik <davidbielik@gmail.com>
2022-01-02 13:34:49 -0500

    grid can change number of columns now

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const number = 256;
+const number = 10000;
 
 for (i=1; i<number+1; i++) {
     const container = document.querySelector('#container');
     const content = document.createElement('div');
     content.classList.add('content');
-    content.textContent = i;
     container.appendChild(content); 
 
 }   
